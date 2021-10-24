490235937c80793c50ad4d72f018705b32aad0e5
Daniela-2017 <danny15zavalalic@gmail.com>
2021-10-24 17:37:53 -0600

    fileSolicitted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     SharedModule,
     FontAwesomeModule,
     BrowserAnimationsModule,
+    
 
   ],
   providers: [],
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,10 +2,37 @@ import { NgModule } from '@angular/core';
 
 import {MatButtonModule} from '@angular/material/button';
 
+import { MatExpansionModule } from '@angular/material/expansion';
+
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+
+
 @NgModule({
   exports:[
-    MatButtonModule
+    MatButtonModule,
+    MatExpansionModule,
+    MatIconModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    
+    
+    
   ]
+  
 
 })
-export class MaterialModule { }
+
+
+export class MaterialModule { 
+  
+ 
+}
+
+
+
+
